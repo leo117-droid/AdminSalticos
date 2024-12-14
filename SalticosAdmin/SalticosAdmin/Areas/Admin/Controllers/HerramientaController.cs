8b137b7a9d9aa62ed7f1193220903ac71afc752b
Camila <caulate@estudiantec.cr>
2024-12-14 11:40:45 -0600

    Creacion del herramienta controller

---
+++ SalticosAdmin/SalticosAdmin/Areas/Admin/Controllers/HerramientaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalticosAdmin.AccesoDeDatos.Repositorio;
+using SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio;
+
+namespace SalticosAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HerramientaController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public HerramientaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Herramienta.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+
+
+        #endregion
+    }
+}
