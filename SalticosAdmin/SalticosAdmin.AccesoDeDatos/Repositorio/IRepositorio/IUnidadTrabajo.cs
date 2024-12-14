956ce2a798dbc6a097596e2fb2605ebd06ae0a7e
Camila <caulate@estudiantec.cr>
2024-12-14 11:15:42 -0600

    Creacion Unidad de Trabajo

---
+++ SalticosAdmin/SalticosAdmin.AccesoDeDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio
+{
+    public interface IUnidadTrabajo : IDisposable
+    {
+        IHerramientaRepositorio Herramienta { get; }
+
+        Task Guardar();
+    }
+
+   
+}
---
+++ SalticosAdmin/SalticosAdmin.AccesoDeDatos/Repositorio/UnidadTrabajo.cs
@@ -0,0 +1,32 @@
+﻿using SalticosAdmin.AccesoDeDatos.Data;
+using SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdmin.AccesoDeDatos.Repositorio
+{
+    public class UnidadTrabajo : IUnidadTrabajo
+    {
+        private readonly ApplicationDbContext _db;
+        public IHerramientaRepositorio Herramienta { get; private set; }
+
+        public UnidadTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            Herramienta = new HerramientaRepositorio(_db);
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public async Task Guardar()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
