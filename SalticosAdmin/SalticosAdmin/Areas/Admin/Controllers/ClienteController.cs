3432f2f38c647f3a640a5ca24702024f3ddb6178
Camila <caulate@estudiantec.cr>
2024-12-15 19:50:53 -0600

    Creacion de vista index y upsert para cliente y contacto

---
+++ SalticosAdmin/SalticosAdmin/Areas/Admin/Controllers/ClienteController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalticosAdmin.AccesoDeDatos.Repositorio;
+using SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio;
+using SalticosAdmin.Modelos;
+using SalticosAdmin.Utilidades;
+
+namespace SalticosAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ClienteController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public ClienteController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Cliente cliente = new Cliente();
+
+
+            if (id == null)
+            {
+                // Crear una nueva herramienta
+                return View(cliente);
+            }
+
+            // Actualizamos
+            cliente = await _unidadTrabajo.Cliente.Obtener(id.GetValueOrDefault());
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                if (cliente.Id == 0)
+                {
+                    await _unidadTrabajo.Cliente.Agregar(cliente);
+                    TempData[DS.Exitosa] = "Cliente creado Exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Cliente.Actualizar(cliente);
+                    TempData[DS.Exitosa] = "Cliente actualizado Exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar cliente";
+            return View(cliente);
+        }
+
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Cliente.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var clienteBd = await _unidadTrabajo.Cliente.Obtener(id);
+            if (clienteBd == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Cliente" });
+            }
+            _unidadTrabajo.Cliente.Remover(clienteBd);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Cliente borrada exitosamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Herramienta.ObtenerTodos();
+            if (id == 0)
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+        }
+
+        #endregion
+    }
+}
---
+++ SalticosAdmin/SalticosAdmin/Areas/Admin/Controllers/ContactoController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalticosAdmin.AccesoDeDatos.Repositorio;
+using SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio;
+using SalticosAdmin.Modelos;
+using SalticosAdmin.Utilidades;
+
+namespace SalticosAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactoController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public ContactoController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Contacto contacto = new Contacto();
+
+
+            if (id == null)
+            {
+                // Crear una nueva herramienta
+                return View(contacto);
+            }
+
+            // Actualizamos
+            contacto = await _unidadTrabajo.Contacto.Obtener(id.GetValueOrDefault());
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return View(contacto);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                if (contacto.Id == 0)
+                {
+                    await _unidadTrabajo.Contacto.Agregar(contacto);
+                    TempData[DS.Exitosa] = "Contacto creado Exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Contacto.Actualizar(contacto);
+                    TempData[DS.Exitosa] = "Contacto actualizado Exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar contacto";
+            return View(contacto);
+        }
+
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Contacto.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var contactoBd = await _unidadTrabajo.Contacto.Obtener(id);
+            if (contactoBd == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Contacto" });
+            }
+            _unidadTrabajo.Contacto.Remover(contactoBd);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Contacto borrado exitosamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Contacto.ObtenerTodos();
+            if (id == 0)
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+        }
+
+        #endregion
+    }
+}
