7f9e257a7c0793a6b28f8b37dc694611d90d33d1
Yosward García <yosward.gar@gmail.com>
2024-12-17 10:34:11 -0600

    Creacion de las vistas de Vehiculos

---
+++ SalticosAdmin/SalticosAdmin/Areas/Admin/Controllers/VehiculoController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalticosAdmin.AccesoDeDatos.Repositorio;
+using SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio;
+using SalticosAdmin.Modelos;
+using SalticosAdmin.Utilidades;
+
+namespace SalticosAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class VehiculoController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public VehiculoController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Vehiculo vehiculo = new Vehiculo();
+
+
+            if (id == null)
+            {
+                // Crear una nueva herramienta
+                return View(vehiculo);
+            }
+
+            // Actualizamos
+            vehiculo = await _unidadTrabajo.Vehiculo.Obtener(id.GetValueOrDefault());
+            if (vehiculo == null)
+            {
+                return NotFound();
+            }
+            return View(vehiculo);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Vehiculo vehiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                if (vehiculo.Id == 0)
+                {
+                    await _unidadTrabajo.Vehiculo.Agregar(vehiculo);
+                    TempData[DS.Exitosa] = "Vehiculo creado Exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Vehiculo.Actualizar(vehiculo);
+                    TempData[DS.Exitosa] = "Vehiculo actualizado Exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar vehiculo";
+            return View(vehiculo);
+        }
+
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Vehiculo.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var vehiculoBd = await _unidadTrabajo.Vehiculo.Obtener(id);
+            if (vehiculoBd == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Vehiculo" });
+            }
+            _unidadTrabajo.Vehiculo.Remover(vehiculoBd);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Vehiculo borrada exitosamente" });
+        }
+
+        [ActionName("ValidarPlaca")]
+        public async Task<IActionResult> ValidarPlaca(string placa, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Vehiculo.ObtenerTodos();
+            if (id == 0)
+            {
+                valor = lista.Any(b => b.Placa.ToLower().Trim() == placa.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Placa.ToLower().Trim() == placa.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+        }
+
+        #endregion
+    }
+}
---
+++ SalticosAdmin/SalticosAdmin/wwwroot/js/vehiculo.js
@@ -0,0 +1,78 @@
+﻿let datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    datatable = $('#tblDatos').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from _MAX_ total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Vehiculo/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "marca", "width": "20%" },
+            { "data": "modelo", "width": "20%" },
+            { "data": "placa", "width": "20%" },
+            { "data": "tipoVehiculo", "width": "20%" },
+
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class = "text-center">
+                            <a href="/Admin/Vehiculo/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer"> 
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onclick = Delete("/Admin/Vehiculo/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer"> 
+                                <i class = "bi bi-trash3-fill"></i>
+                            </a>
+
+                        </div>
+                    `;
+                }, "width": "20%"
+            }
+        ]
+    });
+}
+
+
+function Delete(url) {
+    swal({
+        title: "Esta seguro de Eliminar el Vehículo?",
+        text: "Este registro no se podra recuperar",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((borrar) => {
+        if (borrar) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        datatable.ajax.reload();
+
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
