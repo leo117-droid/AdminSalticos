eca3c7e09c5c5f97dc4ff09865baf2a2fb31ca0d
Yosward García <yosward.gar@gmail.com>
2024-12-19 11:07:49 -0600

    Creacion de vistas de ingrediente

---
+++ SalticosAdmin/SalticosAdmin/Areas/Admin/Controllers/IngredienteController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalticosAdmin.AccesoDeDatos.Repositorio;
+using SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio;
+using SalticosAdmin.Modelos;
+using SalticosAdmin.Utilidades;
+
+namespace SalticosAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class IngredienteController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public IngredienteController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Ingrediente ingrediente = new Ingrediente();
+
+
+            if (id == null)
+            {
+                // Crear una nuevo ingrediente
+                return View(ingrediente);
+            }
+
+            // Actualizamos
+            ingrediente = await _unidadTrabajo.Ingrediente.Obtener(id.GetValueOrDefault());
+            if (ingrediente == null)
+            {
+                return NotFound();
+            }
+            return View(ingrediente);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Ingrediente ingrediente)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ingrediente.Id == 0)
+                {
+                    await _unidadTrabajo.Ingrediente.Agregar(ingrediente);
+                    TempData[DS.Exitosa] = "Ingrediente creado Exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Ingrediente.Actualizar(ingrediente);
+                    TempData[DS.Exitosa] = "Ingrediente actualizado Exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar ingrediente";
+            return View(ingrediente);
+        }
+
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Ingrediente.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ingredienteBd = await _unidadTrabajo.Ingrediente.Obtener(id);
+            if (ingredienteBd == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Ingrediente" });
+            }
+            _unidadTrabajo.Ingrediente.Remover(ingredienteBd);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Ingrediente borrada exitosamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Ingrediente.ObtenerTodos();
+            if (id == 0)
+            {
+                if(nombre != null)
+                {
+                    valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+                }
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+        }
+
+        #endregion
+    }
+}
---
+++ SalticosAdmin/SalticosAdmin/wwwroot/js/ingrediente.js
@@ -0,0 +1,77 @@
+﻿let datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    datatable = $('#tblDatos').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from _MAX_ total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Ingrediente/ObtenerTodos"
+        },
+        "columns": [
+            { "data": "nombre", "width": "20%" },
+            { "data": "descripcion", "width": "40%" },
+            { "data": "precio", "width": "20%" },
+
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class = "text-center">
+                            <a href="/Admin/Ingrediente/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer"> 
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onclick = Delete("/Admin/Ingrediente/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer"> 
+                                <i class = "bi bi-trash3-fill"></i>
+                            </a>
+
+                        </div>
+                    `;
+                }, "width": "20%"
+            }
+        ]
+    });
+}
+
+
+function Delete(url) {
+    swal({
+        title: "Esta seguro de Eliminar el Ingrediente?",
+        text: "Este registro no se podra recuperar",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((borrar) => {
+        if (borrar) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        datatable.ajax.reload();
+
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
