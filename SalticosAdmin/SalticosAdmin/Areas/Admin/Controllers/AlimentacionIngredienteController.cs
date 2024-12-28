c231176cb203f632eb838d774733578ef4ed1aa0
Camila <caulate@estudiantec.cr>
2024-12-27 20:25:13 -0600

    Controller y Vistas de AlimentacionIngrediente

---
+++ SalticosAdmin/SalticosAdmin/Areas/Admin/Controllers/AlimentacionIngredienteController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalticosAdmin.AccesoDeDatos.Repositorio;
+using SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio;
+using SalticosAdmin.Modelos;
+using SalticosAdmin.Modelos.ViewModels;
+using SalticosAdmin.Utilidades;
+
+namespace SalticosAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AlimentacionIngredienteController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+
+        public AlimentacionIngredienteController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            if (id != null)
+            {
+                ViewData["Id"] = id;
+            }
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> Upsert(int alimentacionID, int? relacionId)
+        {
+
+
+            AlimentacionIngredienteVM alimentacionIngredienteVM = new AlimentacionIngredienteVM()
+            {
+                IdAlimentacion = alimentacionID,
+
+                ListaIngrediente = _unidadTrabajo.AlimentacionIngrediente.ObtenerIngrediente("Ingrediente", alimentacionID)
+            };
+
+
+            if (relacionId == null)
+            {
+                return View(alimentacionIngredienteVM);
+            }
+            else
+            {
+
+                alimentacionIngredienteVM.IdRelacion = relacionId.Value;
+                var alimentacionIngredienteOBJ = await _unidadTrabajo.AlimentacionIngrediente.ObtenerPrimero(X => X.Id == alimentacionIngredienteVM.IdRelacion);
+                if (alimentacionIngredienteOBJ == null)
+                {
+                    return NotFound();
+                }
+
+
+                //  productoPrecioVM.tipoPrecioNombre = productoPrecioVM.ListaPrecios.Where(x => x.Value.Equals(productoPrecioOBJ.Idprecio.ToString())).Select(x=>x.Text).FirstOrDefault();
+                alimentacionIngredienteVM.IdIngrediente = alimentacionIngredienteOBJ.IdIngrediente;
+
+
+                return View(alimentacionIngredienteVM);
+            }
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(AlimentacionIngredienteVM alimentacionIngredienteVM)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                var allErrors = ModelState.Values
+                               .SelectMany(v => v.Errors)
+                               .Select(e => e.ErrorMessage)
+                               .ToList();
+
+                foreach (var error in allErrors)
+                {
+                    Console.WriteLine(error);
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                if (alimentacionIngredienteVM.IdRelacion == 0)
+                {
+                    //productoPrecioVM.productoPrecio.Idproducto = id;
+                    AlimentacionIngrediente existeIngrediente = await _unidadTrabajo.AlimentacionIngrediente.ObtenerPrimero(X => X.IdAlimentacion == alimentacionIngredienteVM.IdAlimentacion && X.IdIngrediente == alimentacionIngredienteVM.IdIngrediente);
+                    if (existeIngrediente != null)
+                    {
+                        TempData[DS.Error] = "ingrediente ya existente";
+                        
+                    }
+                    else
+                    {
+                        var alimentacionIngrediente = new AlimentacionIngrediente();
+                        alimentacionIngrediente.IdAlimentacion = alimentacionIngredienteVM.IdAlimentacion;
+                        alimentacionIngrediente.IdIngrediente = alimentacionIngredienteVM.IdIngrediente;
+
+                        alimentacionIngrediente.Ingrediente = await _unidadTrabajo.Ingrediente.ObtenerPrimero(X => X.Id == alimentacionIngredienteVM.IdIngrediente);
+                        alimentacionIngrediente.Alimentacion = await _unidadTrabajo.Alimentacion.ObtenerPrimero(X => X.Id == alimentacionIngredienteVM.IdAlimentacion);
+
+                        await _unidadTrabajo.AlimentacionIngrediente.Agregar(alimentacionIngrediente);
+
+                        TempData[DS.Exitosa] = "Ingrediente agregado exitosamente";
+
+                       
+                    }
+
+                }
+                else
+                {
+
+                    var alimentacionIngrediente = new AlimentacionIngrediente();
+                    alimentacionIngrediente.IdAlimentacion = alimentacionIngredienteVM.IdAlimentacion;
+                    alimentacionIngrediente.IdIngrediente = alimentacionIngredienteVM.IdIngrediente;
+                    alimentacionIngrediente.Id = alimentacionIngredienteVM.IdRelacion;
+
+                    alimentacionIngrediente.Ingrediente = await _unidadTrabajo.Ingrediente.ObtenerPrimero(X => X.Id == alimentacionIngredienteVM.IdIngrediente);
+                    alimentacionIngrediente.Alimentacion = await _unidadTrabajo.Alimentacion.ObtenerPrimero(X => X.Id == alimentacionIngredienteVM.IdAlimentacion);
+
+                    _unidadTrabajo.AlimentacionIngrediente.Actualizar(alimentacionIngrediente);
+                    TempData[DS.Exitosa] = "Ingrediente actualizado exitosamente";
+
+
+                }
+                await _unidadTrabajo.Guardar();
+                string returnUrl = Url.Action("Index", "AlimentacionIngrediente", new { id = alimentacionIngredienteVM.IdAlimentacion });
+                return Redirect(returnUrl);
+            }
+            var mensajeError = "Error al grabar ingrediente a producto de alimentacion";
+            TempData[DS.Error] = mensajeError;
+            return View(alimentacionIngredienteVM);
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            int id = 0;
+            if (HttpContext.Request.Query.ContainsKey("id"))
+            {
+                int.TryParse(HttpContext.Request.Query["id"], out id);
+            }
+
+            var todos = await _unidadTrabajo.AlimentacionIngrediente.ObtenerTodos(incluirPropiedades: "Ingrediente");
+
+            var filtrados = todos.Where(t => t.IdAlimentacion == id);
+            return Json(new { data = filtrados });
+
+        }
+
+        
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var AlimentacionIngredienteBd = await _unidadTrabajo.AlimentacionIngrediente.Obtener(id);
+            if(AlimentacionIngredienteBd == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Ingrediente de producto de Alimentación" });
+            }
+
+            _unidadTrabajo.AlimentacionIngrediente.Remover(AlimentacionIngredienteBd);
+            await _unidadTrabajo.Guardar();
+
+            return Json(new { success = true, message = "Ingrediente borrado de producto de alimentación exitosamente" });
+
+        }
+
+
+
+        #endregion
+    }
+}
