a88d755a54e7c489f3aa689c597ad16c99cb9e92
Camila <caulate@estudiantec.cr>
2024-12-28 21:09:02 -0600

    Agregar Modelo y Configuracion de EventoVehiculo

---
+++ SalticosAdmin/SalticosAdmin.Modelos/EventoVehiculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdmin.Modelos
+{
+    public class EventoVehiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int IdVehiculo { get; set; }
+
+        [ForeignKey("IdVehiculo")]
+        public Vehiculo Vehiculo { get; set; }
+
+        public int IdEvento { get; set; }
+
+        [ForeignKey("IdEvento")]
+        public Evento Evento { get; set; }
+
+        [NotMapped] // Nose agrega a la base de datos
+        public string? NombreVehiculo { get; set; }
+    }
+}
