af58929e0fe446292afe0a69822db2ec8c9a3064
Yosward García <yosward.gar@gmail.com>
2024-12-29 21:34:44 -0600

    Creacion de repositorio y viewmodel de EventoServicioAdicional

---
+++ SalticosAdmin/SalticosAdmin.AccesoDeDatos/Repositorio/IRepositorio/IEventoServicioAdicionalRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SalticosAdmin.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdmin.AccesoDeDatos.Repositorio.IRepositorio
+{
+    public interface IEventoServicioAdicionalRepositorio : IRepositorio<EventoServicioAdicional>
+    {
+        void Actualizar(EventoServicioAdicional eventoServicioAdicional);
+
+
+        IEnumerable<SelectListItem> ObtenerServicio(string objeto, int? idEvento);
+
+    }
+}
