59ce5801feff14436d574c25f2932fb107ddfdf1
Camila <caulate@estudiantec.cr>
2025-01-14 12:01:42 -0600

    Inicio en informe de inflable

---
+++ SalticosAdmin/SalticosAdmin/Areas/Admin/Controllers/InformeController.cs
@@ -0,0 +1,60 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+using SalticosAdmin.Modelos;
+using SalticosAdmin.Servicios;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.IO;
+
+namespace SalticosAdmin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class InformeController : Controller
+    {
+        private readonly InformeServicio _informeServicio;
+
+        public InformeController(InformeServicio informeServicio)
+        {
+            _informeServicio = informeServicio;
+        }
+
+        // Acción para generar el informe
+        public IActionResult GenerarInformeInflables()
+        {
+            // Obtener inflables más utilizados
+            List<Inflable> inflablesMasUtilizados = _informeServicio.ObtenerInflablesMasUtilizados();
+
+            // Crear el documento PDF
+            using (var memoryStream = new MemoryStream())
+            {
+                var document = new Document(PageSize.A4);
+                PdfWriter.GetInstance(document, memoryStream);
+                document.Open();
+
+                // Título del informe
+                document.Add(new Paragraph("Informe de Inflables Más Utilizados"));
+                document.Add(new Paragraph($"Fecha: {DateTime.Now.ToShortDateString()}"));
+                document.Add(new Paragraph(" "));
+
+                // Crear la tabla para mostrar los inflables
+                PdfPTable table = new PdfPTable(2);
+                table.AddCell("Nombre del Inflable");
+                table.AddCell("Cantidad de Veces Utilizado");
+
+                // Agregar los inflables a la tabla
+                foreach (var inflable in inflablesMasUtilizados)
+                {
+                    table.AddCell(inflable.Nombre);
+                    table.AddCell(inflable.Cantidad.ToString());
+                }
+
+                document.Add(table);
+                document.Close();
+
+                // Descargar el PDF
+                return File(memoryStream.ToArray(), "application/pdf", "Informe_Inflables.pdf");
+            }
+        }
+    }
+}
---
+++ SalticosAdmin/SalticosAdmin/Servicios/InformeServicio.cs
@@ -0,0 +1,40 @@
+﻿using SalticosAdmin.AccesoDeDatos.Data;
+using SalticosAdmin.Modelos;
+
+namespace SalticosAdmin.Servicios
+{
+    public class InformeServicio
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InformeServicio(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Método para obtener los inflables más utilizados
+        public List<Inflable> ObtenerInflablesMasUtilizados()
+        {
+            var inflablesUtilizados = _context.EventoInflable
+                .GroupBy(ei => ei.IdInflable)  // Agrupar por Inflable
+                .Select(g => new
+                {
+                    InflableId = g.Key,
+                    Cantidad = g.Count()  // Contar cuántas veces ha sido utilizado
+                })
+                .OrderByDescending(x => x.Cantidad)  // Ordenar por la cantidad
+                .ToList();
+
+            // Obtener los inflables con sus nombres y cantidades
+            var inflables = inflablesUtilizados.Select(x =>
+            {
+                var inflable = _context.Inflables.FirstOrDefault(i => i.Id == x.InflableId);
+                inflable.Nombre = inflable.Nombre;  // Asignar el nombre al inflable
+                return inflable;
+            }).ToList();
+
+            return inflables;
+        }
+    }
+
+}
