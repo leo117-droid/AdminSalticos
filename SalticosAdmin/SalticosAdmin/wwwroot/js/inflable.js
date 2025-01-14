d77884652d6437333208a540134d5b76a61a2cf1
leo117-droid <leomora641@gmail.com>
2025-01-14 11:16:39 -0600

    Cambios simbologia colones

--- SalticosAdmin/SalticosAdmin/wwwroot/js/inflable.js
+++ SalticosAdmin/SalticosAdmin/wwwroot/js/inflable.js
@@ -47,13 +47,13 @@ function loadDataTable() {
             {
                 "data": "precio", "className": "text-end",
                 "render": function (data) {
-                    var d = data.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+                    var d = `₡${data.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
                     return d
                 } },
             {
                 "data": "precioHoraAdicional", "className": "text-end",
                 "render": function (data) {
-                    var d = data.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+                    var d = `₡${data.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
                     return d
                 } },
             { "data": "categoriaTamanno.nombre" },
