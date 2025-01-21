d62533c9fba5223cc61a22a6686767cf4be69c74
Fabiana0744 <fabiana0744@hotmail.com>
2025-01-21 14:37:19 -0600

    Dropdown en tabla de evento para las acciones

--- SalticosAdmin/SalticosAdmin/wwwroot/js/evento.js
+++ SalticosAdmin/SalticosAdmin/wwwroot/js/evento.js
@@ -33,62 +33,51 @@ function loadDataTable() {
             {
                 "data": "horaInicio",
                 "render": function (data) {
-                    return moment(data, "HH:mm:ss").format("hh:mm A"); 
+                    return moment(data, "HH:mm:ss").format("hh:mm A");
                 }
             },
             {
                 "data": "horaFinal",
                 "render": function (data) {
-                    return moment(data, "HH:mm:ss").format("hh:mm A"); 
+                    return moment(data, "HH:mm:ss").format("hh:mm A");
                 }
             },
-            { "data": "direccion"},
-            { "data": "provincia"},
+            { "data": "direccion" },
+            { "data": "provincia" },
             {
-                "data": null, 
+                "data": null,
                 "render": function (data) {
                     return `${data.cliente.nombre} ${data.cliente.apellidos}`;
                 }
             },
-
             {
                 "data": "id",
                 "render": function (data) {
                     return `
-                        <div class = "text-center">
+                        <div class="text-center">
+                            <!-- Botón Editar fuera del dropdown -->
                             <a href="/Admin/Evento/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer"> 
                                 <i class="bi bi-pencil-square"></i>
                             </a>
-
-                            <a href="/Admin/EventoAlimentacion/Index/${data}" class="btn btn-primary btn-secondary btn-outline-white" style="cursor:pointer">
-                                <i class="bi bi-cart"></i> Alimentacion
-                            </a>
-
-                            <a href="/Admin/EventoVehiculo/Index/${data}" class="btn btn-primary btn-secondary btn-outline-white" style="cursor:pointer">
-                                <i class="bi bi-car-front-fill"></i> Vehiculo
-                            </a>
-
-                            <a href="/Admin/EventoMobiliario/Index/${data}" class="btn btn-primary btn-secondary btn-outline-white" style="cursor:pointer">
-                                <i class="bi bi-wrench"></i> Mobiliario
-                            </a>
-
-
-                            <a href="/Admin/EventoPersonal/Index/${data}" class="btn btn-primary btn-secondary btn-outline-white" style="cursor:pointer">
-                                <i class="bi bi-person-fill-add"></i> Personal
-                            </a>
-
-                            <a href="/Admin/EventoInflable/Index/${data}" class="btn btn-primary btn-secondary btn-outline-white" style="cursor:pointer">
-                                <i class="bi bi-balloon-fill"></i> Inflable
-                            </a>
-
-                            <a href="/Admin/EventoServicioAdicional/Index/${data}" class="btn btn-primary btn-secondary btn-outline-white" style="cursor:pointer">
-                                <i class="bi bi-plus-square-dotted"></i> Servicios Adicionales
-                            </a>
+                            
+                            <!-- Dropdown para las demás acciones -->
+                            <div class="dropdown d-inline-block">
+                                <button class="btn btn-primary btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-gear"></i> Acciones
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <li><a class="dropdown-item" href="/Admin/EventoAlimentacion/Index/${data}"><i class="bi bi-cart"></i> Alimentación</a></li>
+                                    <li><a class="dropdown-item" href="/Admin/EventoVehiculo/Index/${data}"><i class="bi bi-car-front-fill"></i> Vehículo</a></li>
+                                    <li><a class="dropdown-item" href="/Admin/EventoMobiliario/Index/${data}"><i class="bi bi-wrench"></i> Mobiliario</a></li>
+                                    <li><a class="dropdown-item" href="/Admin/EventoPersonal/Index/${data}"><i class="bi bi-person-fill-add"></i> Personal</a></li>
+                                    <li><a class="dropdown-item" href="/Admin/EventoInflable/Index/${data}"><i class="bi bi-balloon-fill"></i> Inflable</a></li>
+                                    <li><a class="dropdown-item" href="/Admin/EventoServicioAdicional/Index/${data}"><i class="bi bi-plus-square-dotted"></i> Servicios Adicionales</a></li>
+                                </ul>
+                            </div>
 
                             <a onclick = Delete("/Admin/Evento/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer"> 
                                 <i class = "bi bi-trash3-fill"></i>
                             </a>
-
                         </div>
                     `;
                 },
