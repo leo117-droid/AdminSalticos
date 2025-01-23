bbf9f5f6bed1064b0c3d3acd887dac5022f19f03
Camila <caulate@estudiantec.cr>
2025-01-23 15:19:32 -0600

    Cambios ortografía modelos cliente y contacto

--- SalticosAdmin/SalticosAdmin.Modelos/Contacto.cs
+++ SalticosAdmin/SalticosAdmin.Modelos/Contacto.cs
@@ -13,24 +13,24 @@ namespace SalticosAdmin.Modelos
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Nombre es requerido")]
-        [MaxLength(50, ErrorMessage = "Nombre debe ser maximo 50 caracteres")]
+        [MaxLength(50, ErrorMessage = "Nombre debe ser máximo 50 caracteres")]
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "Apellido es requerido")]
-        [MaxLength(50, ErrorMessage = "Apellido debe ser maximo 50 caracteres")]
+        [MaxLength(50, ErrorMessage = "Apellido debe ser máximo 50 caracteres")]
         public string Apellido { get; set; }
 
         [Required(ErrorMessage = "Tipo Servicio es requerido")]
         [MaxLength(50, ErrorMessage = "Tipo Servicio debe ser maximo 50 caracteres")]
         public string TipoServicio { get; set; }
 
-        [Required(ErrorMessage = "Direccion es requerido")]
-        [MaxLength(100, ErrorMessage = "Direccion debe ser maximo 100 caracteres")]
+        [Required(ErrorMessage = "Dirección es requerido")]
+        [MaxLength(100, ErrorMessage = "Dirección debe ser máximo 100 caracteres")]
         public string Direccion { get; set; }
 
-        [Required(ErrorMessage = "Telefono es requerido")]
-        [StringLength(8, ErrorMessage = "El telefono debe tener exactamente 8 caracteres", MinimumLength = 8)]
-        [RegularExpression(@"^\d+$", ErrorMessage = "El Telefono solo debe contener números")]
+        [Required(ErrorMessage = "Teléfono es requerido")]
+        [StringLength(8, ErrorMessage = "El teléfono debe tener exactamente 8 caracteres", MinimumLength = 8)]
+        [RegularExpression(@"^\d+$", ErrorMessage = "El teléfono solo debe contener números")]
         public string Telefono { get; set; }
 
         [Required(ErrorMessage = "Correo es requerido")]
