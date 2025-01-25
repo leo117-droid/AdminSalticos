b761e9148bdc176a784a3d9ae3a421e6b5b0f326
Yosward García <yosward.gar@gmail.com>
2025-01-25 15:17:38 -0600

    Creación de proyecto de pruebas automatizadas

---
+++ SalticosAdmin/SalticosAdminAutomatedTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SalticosAdminAutomatedTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
