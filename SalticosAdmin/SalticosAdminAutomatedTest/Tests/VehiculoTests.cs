7652500d8726ac36fdf837d4fab7b51ebf9a931a
Yosward García <yosward.gar@gmail.com>
2025-01-25 15:24:11 -0600

    Creación de carpeta para las pages y tests

---
+++ SalticosAdmin/SalticosAdminAutomatedTest/Tests/VehiculoTests.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdminAutomatedTest.Tests
+{
+    public class VehiculoTests
+    {
+        private IWebDriver driver;
+
+
+
+    }
+}
