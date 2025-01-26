7e7d53c87bbf3f5c56ac313a8da52f257a184061
Yosward García <yosward.gar@gmail.com>
2025-01-26 11:44:48 -0600

    Pruebas automatizadas de categoria tamaño

---
+++ SalticosAdmin/SalticosAdminAutomatedTest/Pages/CategoriaTamannoPage.cs
@@ -0,0 +1,129 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdminAutomatedTest.Pages
+{
+    public class CategoriaTamannoPage : Base
+    {
+        // Entrar gestión categoría tamaño
+        By IrServiciosBtnLocator = By.XPath("/html/body/div/main/section/div/div[1]/div[3]/div/div/a");
+        By IrInflablesBtnLocator = By.XPath("/html/body/div/main/section/div/div[1]/div[1]/div/div/a");
+        By IrCategoriaTamannoBtnLocator = By.XPath("/html/body/div/main/section/div/div/div[3]/div/div/a");
+
+        // Crear categoría tamaño
+        By CrearNuevaCategoriaTamannoBtnLocator = By.XPath("/html/body/div/main/div[1]/div[2]/a");
+        By NombreInputLocator = By.Id("nombre");
+        By CrearBtnLocator = By.XPath("//*[@id=\"formCategoriaTamanno\"]/div/div[4]/div/button");
+
+        By BuscarInputLocator = By.XPath("//*[@id=\"tblDatos_filter\"]/label/input");
+        By NombreRegistroLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr/td[1]");
+
+        By MsgNombreRepetidoLocator = By.XPath("/html/body/div[2]/div/div[3]");
+
+        // Actualizar categoría tamaño
+        By EditarBtnLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr[1]/td[2]/div/a[1]/i");
+        By GuardarCambiosBtnLocator = By.XPath("//*[@id=\"formCategoriaTamanno\"]/div/div[4]/div/button");
+
+        // Eliminar
+        By EliminarBtnLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr[1]/td[2]/div/a[2]/i");
+        By MensajeEliminarBtnLocator = By.XPath("/html/body/div[2]/div/div[4]/div[2]/button");
+        By NingunRegistroMsgLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr/td");
+
+
+        public CategoriaTamannoPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+
+        public void IniciarSesion(String usuario, String contrasenna)
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Login(usuario, contrasenna);
+
+        }
+
+
+        public void GestionCategoriaTamanno()
+        {
+            ClickElementUsingJS(IrServiciosBtnLocator);
+            Thread.Sleep(2000);
+            Click(IrInflablesBtnLocator);
+            Thread.Sleep(2000);
+            Click(IrCategoriaTamannoBtnLocator);
+        }
+
+
+        public void AgregarCategoriaTamanno(String nombre)
+        {
+            Click(CrearNuevaCategoriaTamannoBtnLocator);
+            Thread.Sleep(2000);
+            Type(nombre, NombreInputLocator);
+            Thread.Sleep(2000);
+
+            Click(CrearBtnLocator);
+        }
+
+
+        public Boolean CategoriaTamannoEstaRegistrada(String nombre)
+        {
+            Type(nombre, BuscarInputLocator);
+            return IsDisplayed(NombreRegistroLocator) && GetText(NombreRegistroLocator) == nombre;
+        }
+
+
+        public void RegistrarCategoriaTamannoConNombreRepetido(String nombre)
+        {
+            Click(CrearNuevaCategoriaTamannoBtnLocator);
+            Thread.Sleep(2000);
+            Type(nombre + "\n", NombreInputLocator);
+
+        }
+
+        public Boolean MensajeErrorNombreRepetidoDesplegado()
+        {
+            return IsDisplayed(MsgNombreRepetidoLocator);
+        }
+
+
+
+        public void ActualizarNombreCategoriaTamanno(String nombre, String nuevoNombre)
+        {
+            Type(nombre, BuscarInputLocator);
+
+            Click(EditarBtnLocator);
+            Thread.Sleep(2000);
+            Clear(NombreInputLocator);
+            Type(nuevoNombre, NombreInputLocator);
+            Click(GuardarCambiosBtnLocator);
+
+        }
+
+        public Boolean NombreCategoriaTamannoActualizado(String nombre)
+        {
+            Type(nombre, BuscarInputLocator);
+            return GetText(NombreRegistroLocator) == nombre;
+        }
+
+
+        public void EliminarCategoriaTamanno(String nombre)
+        {
+            Type(nombre, BuscarInputLocator);
+            Click(EliminarBtnLocator);
+            Click(MensajeEliminarBtnLocator);
+            Thread.Sleep(2000);
+
+        }
+
+
+
+        public Boolean EstaCategoriaTamannoEliminado()
+        {
+            return IsDisplayed(NingunRegistroMsgLocator);
+        }
+    }
+}
---
+++ SalticosAdmin/SalticosAdminAutomatedTest/Tests/CategoriaTamannoTests.cs
@@ -0,0 +1,78 @@
+﻿using OpenQA.Selenium;
+using SalticosAdminAutomatedTest.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdminAutomatedTest.Tests
+{
+    public class CategoriaTamannoTests
+    {
+        private IWebDriver driver;
+
+        CategoriaTamannoPage categoriaTamannoPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            categoriaTamannoPage = new CategoriaTamannoPage(driver);
+            driver = categoriaTamannoPage.ChromeDriverConnection();
+            driver.Manage().Window.Maximize();
+            categoriaTamannoPage.Visit("http://localhost:5270/");
+
+            categoriaTamannoPage.IniciarSesion("yoswi200210@gmail.com", "Hola321!");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+
+        [Test, Order(1)]
+        public void CrearCategoriaTamannooConDatosValidos_EsRegistrado()
+        {
+            categoriaTamannoPage.GestionCategoriaTamanno();
+            Thread.Sleep(2000);
+            categoriaTamannoPage.AgregarCategoriaTamanno("Mediano");
+            Thread.Sleep(2000);
+            Assert.IsTrue(categoriaTamannoPage.CategoriaTamannoEstaRegistrada("Mediano"));
+        }
+
+
+        [Test, Order(2)]
+        public void CrearCategoriaTamannooConNombreExistente_MuestraMensajeDeError()
+        {
+            categoriaTamannoPage.GestionCategoriaTamanno();
+            Thread.Sleep(2000);
+            categoriaTamannoPage.RegistrarCategoriaTamannoConNombreRepetido("Mediano");
+            Thread.Sleep(2000);
+            Assert.IsTrue(categoriaTamannoPage.MensajeErrorNombreRepetidoDesplegado());
+        }
+
+
+        [Test, Order(3)]
+        public void ActualizarNombreCategoriaTamanno_EsActualizado()
+        {
+            categoriaTamannoPage.GestionCategoriaTamanno();
+            Thread.Sleep(2000);
+            categoriaTamannoPage.ActualizarNombreCategoriaTamanno("Mediano", "Muy grande");
+            Thread.Sleep(2000);
+            Assert.IsTrue(categoriaTamannoPage.NombreCategoriaTamannoActualizado("Muy grande"));
+        }
+
+
+        [Test, Order(4)]
+        public void EliminarCategoriaTamanno_EsEliminado()
+        {
+            categoriaTamannoPage.GestionCategoriaTamanno();
+            Thread.Sleep(2000);
+            categoriaTamannoPage.EliminarCategoriaTamanno("Muy grande");
+            Thread.Sleep(2000);
+            Assert.IsTrue(categoriaTamannoPage.EstaCategoriaTamannoEliminado());
+        }
+    }
+}
