effe148a103a2880c0dfe410685e47feff44b9de
Yosward García <yosward.gar@gmail.com>
2025-01-27 16:52:05 -0600

    Pruebas automatizadas de mobiliario

---
+++ SalticosAdmin/SalticosAdminAutomatedTest/Pages/MobiliarioPage.cs
@@ -0,0 +1,121 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdminAutomatedTest.Pages
+{
+    public class MobiliarioPage : Base
+    {
+
+        By IrServiciosBtnLocator = By.XPath("/html/body/div/main/section/div/div[1]/div[3]/div/div/a");
+        By IrMobiliarioBtnLocator = By.XPath("/html/body/div/main/section/div/div[1]/div[2]/div/div/a");
+
+        By CrearNuevoMobiliarioLocator = By.XPath("/html/body/div/main/div[1]/div[2]/a");
+        By NombreInputLocator = By.Id("nombre");
+        By DescripcionInputLocator = By.Id("Descripcion");
+        By DimensionesInputLocator = By.Id("Dimensiones");
+        By ImagenInputLocator = By.Id("imagenId");
+        By PrecioInputLocator = By.Id("Precio");
+        By InventarioInputLocator = By.Id("Inventario");
+        By CrearBtnLocator = By.XPath("//*[@id=\"formMobiliario\"]/div/div[2]/div[1]/div/div[7]/div/button");
+
+        By BuscarInputLocator = By.XPath("//*[@id=\"tblDatos_filter\"]/label/input");
+        By NombreRegistroLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr[1]/td[1]");
+
+        By EditarBtnLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr[1]/td[6]/div/a[1]/i");
+        By GuardarCambiosLocator = By.XPath("//*[@id=\"formMobiliario\"]/div/div[2]/div[1]/div/div[7]/div/button");
+        By DescripcionRegistroLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr[1]/td[2]");
+
+        By EliminarBtnLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr[1]/td[6]/div/a[2]/i");
+        By EliminarConfirmacionBtnLocator = By.XPath("/html/body/div[2]/div/div[4]/div[2]/button");
+        By NingunRegistroLocator = By.XPath("//*[@id=\"tblDatos\"]/tbody/tr/td");
+
+
+        public MobiliarioPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+
+        public void IniciarSesion(String usuario, String contrasenna)
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.Login(usuario, contrasenna);
+
+        }
+
+
+        public void GestionMobiliario()
+        {
+            ClickElementUsingJS(IrServiciosBtnLocator);
+            Thread.Sleep(1500);
+            ClickElementUsingJS(IrMobiliarioBtnLocator);
+            Thread.Sleep(1500);
+        }
+
+
+        public void CrearMobiliario(String nombre, String descripcion, String dimensiones, String imagen,
+            String precio, String inventario)
+        {
+            Click(CrearNuevoMobiliarioLocator);
+            Thread.Sleep(1500);
+            Type(nombre, NombreInputLocator);
+            Type(descripcion, DescripcionInputLocator);
+            Type(dimensiones, DimensionesInputLocator);
+            Type(imagen, ImagenInputLocator);
+            Clear(PrecioInputLocator);
+            Type(precio, PrecioInputLocator);
+            Clear(InventarioInputLocator);
+            Type(inventario, InventarioInputLocator);
+            Click(CrearBtnLocator);
+        }
+
+
+        public Boolean MobiliarioEstaRegistrado(String nombre)
+        {
+            Type(nombre, BuscarInputLocator);
+            Thread.Sleep(1500);
+            return IsDisplayed(NombreRegistroLocator);
+        }
+
+
+        public void ActualizarDescripcionMobiliario(String nombre, String nuevaDescripcion)
+        {
+            Type(nombre, BuscarInputLocator);
+            Thread.Sleep(1500);
+            Click(EditarBtnLocator);
+            Thread.Sleep(1500);
+            Clear(DescripcionInputLocator);
+            Type(nuevaDescripcion, DescripcionInputLocator);
+            Click(GuardarCambiosLocator);
+        }
+
+
+        public Boolean DescripcionMobiliarioActualizada(String nombre, String nuevaDescripcion)
+        {
+            Type(nombre, BuscarInputLocator);
+            Thread.Sleep(1500);
+            return IsDisplayed(DescripcionRegistroLocator) 
+                && GetText(DescripcionRegistroLocator) == nuevaDescripcion;
+        }
+
+
+        public void EliminarMobiliario(String nombre)
+        {
+            Type(nombre, BuscarInputLocator);
+            Thread.Sleep(1500);
+            Click(EliminarBtnLocator);
+            Thread.Sleep(1500);
+            Click(EliminarConfirmacionBtnLocator);
+        }
+
+        public Boolean MobiliarioEliminado()
+        {
+            return IsDisplayed(NingunRegistroLocator);
+        }
+
+    }
+}
---
+++ SalticosAdmin/SalticosAdminAutomatedTest/Tests/MobiliarioTests.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using SalticosAdminAutomatedTest.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdminAutomatedTest.Tests
+{
+    public class MobiliarioTests
+    {
+
+        private IWebDriver driver;
+
+        MobiliarioPage mobiliarioPage;
+
+        private readonly String imagenPath = @"C:\Users\yaira\Downloads\Imagenes\mesa_conferencia.jpg";
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            mobiliarioPage = new MobiliarioPage(driver);
+            driver = mobiliarioPage.ChromeDriverConnection();
+            driver.Manage().Window.Maximize();
+            mobiliarioPage.Visit("http://localhost:5270/");
+
+            mobiliarioPage.IniciarSesion("yoswi200210@gmail.com", "Hola321!");
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+
+        [Test, Order(1)]
+        public void CrearNuevoMobilairioConDatosValidos()
+        {
+            mobiliarioPage.GestionMobiliario();
+            mobiliarioPage.CrearMobiliario("Mesa de conferencias", "Mesa grande de madera para conferencias", "2m x 1m x 1.5m",
+                imagenPath, "1000", "10");
+            Thread.Sleep(1500);
+
+            Assert.IsTrue(mobiliarioPage.MobiliarioEstaRegistrado("Mesa de conferencias"));
+        }
+
+
+        [Test, Order(2)]
+        public void ActualizarDescripcionMobiliario()
+        {
+            mobiliarioPage.GestionMobiliario();
+            mobiliarioPage.ActualizarDescripcionMobiliario("Mesa de conferencias", "Mesa grande de plástico para conferencias");
+            Thread.Sleep(1500);
+
+            Assert.IsTrue(mobiliarioPage.DescripcionMobiliarioActualizada("Mesa de conferencias", "Mesa grande de plástico para conferencias"));
+        }
+
+
+        [Test, Order(3)]
+        public void EliminarMobiliario()
+        {
+            mobiliarioPage.GestionMobiliario();
+            mobiliarioPage.EliminarMobiliario("Mesa de conferencias");
+            Thread.Sleep(1500);
+
+            Assert.IsTrue(mobiliarioPage.MobiliarioEliminado());
+        }
+    }
+}
