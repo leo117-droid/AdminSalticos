8626ad6a3631fe138f0f11cb9d92681008d3f44c
Yosward García <yosward.gar@gmail.com>
2025-01-27 09:35:29 -0600

    Pruebas automatizadas para bitácora

---
+++ SalticosAdmin/SalticosAdminAutomatedTest/Tests/BitacoraTests.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using SalticosAdminAutomatedTest.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalticosAdminAutomatedTest.Tests
+{
+    public class BitacoraTests
+    {
+
+        private IWebDriver driver;
+
+        BitacoraPage bitacoraPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            bitacoraPage = new BitacoraPage(driver);
+            driver = bitacoraPage.ChromeDriverConnection();
+            driver.Manage().Window.Maximize();
+            bitacoraPage.Visit("http://localhost:5270/");
+
+            bitacoraPage.IniciarSesion("yoswi200210@gmail.com", "Hola321!");
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+
+        [Test, Order(1)]
+        public void FiltrarBitacoraConFechasValidas_EsFiltrado()
+        {
+            bitacoraPage.GestionBitacora();
+            Thread.Sleep(1500);
+            bitacoraPage.FiltrarBitacora("22/01/2025", "23/01/2025");
+            Thread.Sleep(1500);
+            Assert.IsTrue(bitacoraPage.FiltraResultados("22/01/2025", "23/01/2025"));
+        }
+    }
+}
